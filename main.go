ba57d41d22096d79ad2927fcc843206dabc842a7
Nattapong Wongtakai <nattapong@emetworks.com>
2023-11-24 15:04:35 +0700

    gin simple api

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+	r.GET("/ping", pingpongHandler)
+
+	r.Run(":8080")
+}
+
+func pingpongHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
